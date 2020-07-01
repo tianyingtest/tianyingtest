1d1c82bcd998475c0bf69a29be52060f50c8a60e
tianying <1287010274@qq.com>
2020-07-01 13:27:45 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,98 @@
+
+"""
+
+print("你好世界！")
+print("hello word!")
+print("a+b" )
+"""
+
+
+"""a= input("请输入a的值： ")
+b= input("请输入b的值： ")
+print("input获取的内容: ",a+b)"""
+"""
+a=type(23333)
+print(a)
+"""
+
+# print("a")
+# print(type("aaaa"))
+# print(type(()))
+
+# print(type(True))
+# print(type([]))
+# print(type({}))
+# a= int("2")
+# print(type(a))
+# b= str(2222)
+# print(type(b))
+
+
+
+
+# tianying= "aaaaa"
+# print(tianying)
+
+# c= int(222)
+# print(type(c))
+
+
+
+
+
+
+# t= int("aaaa")
+# print(type(t))
+
+
+
+
+
+# a= int("一")
+# print(type(a))
+
+
+
+# a= input("请输入a的值： ")
+# b= input("请输入b的值： ")
+# print("input获取的内容： "，a+b)
+
+
+
+# a= int (input ("请输入a:"))
+# b= int (input ("请输入b:"))
+# print("input获取的内容：",a+b)
+
+
+
+# c= float (input("请输入c:"))
+# d= float (input("请输入d:"))
+# print("input的内容：",c+d)
+
+# a=1111111111111111
+# print(len(a))
+
+
+
+
+
+
+
+"""
+
+
+
+# """
+# a="nihaoshijie"
+# b="wohenhaojiayou"
+# print(len(a))
+# print(len(b))
+# print(len(a+b))
+
+
+a= input("请输入：")
+b= input("请输入：")
+
+print()
+
+
