776062f37afdc44b0356c3de430348749b7ef60b
tianying <1287010274@qq.com>
2020-07-01 20:22:30 +0800

    2020.7.1

--- demo01.py
+++ demo01.py
@@ -90,9 +90,116 @@ print(a)
 # print(len(a+b))
 
 
-a= input("请输入：")
-b= input("请输入：")
-
-print("input获取内容：",len(a+b))
+# 
 
 
+
+# print(type(()))
+
+# 元组
+# a= (1,2,3,5,5,5,5,5,5,5,"哈哈","喜喜",True)
+# d= (2,3,5,6)
+# print(a)
+# print(a.index("喜喜"))
+# # print(a[4])
+
+# print(a.index("喜喜"))
+
+# print(a.count(a)) 
+
+# # 在元组里面又添加了一个元组叫二维元组
+# b= (a,"喜喜","哈哈")
+# c= (a,b,d)
+
+# print (a[5])
+
+
+# # 切片 
+
+# print(a[0:4])  
+# # 左闭右开
+
+
+
+
+# a= [1,2.3,5,6,"aaaa","aaa"]
+# a.append("yigaowoligao")
+# print(a)
+
+# print(a.index(2.3))
+
+# a.insert(0,"insert")
+# print(a)
+
+# a.append(2)
+# print(a)
+
+# a.insert(2,2)
+# print(a)
+
+# b = a.pop(5)
+# print(b)
+
+# xx=["aaa","sdsdsd",1,3]
+# a.extend(xx)
+# print(a)
+# a.insert(0,"sd")
+# print(a)
+
+# a.remove("aaa")
+
+# print(a)
+"""
+字典的特点
+1.字典中的值没有顺序
+2.字典的结构必须是键值对的结构 {key:value}
+
+"""
+
+# a= {"name":"张三",1:"李四","age":25}
+
+# # 取值
+# print(a["name"])
+# # 新增
+# a["high"]="183"
+# print(a)
+# # 修改
+# a["name"]="奥里给"
+# print(a)
+
+# b =a.get("name")
+# print(b)
+
+# c =a.pop("name")
+# print(c)
+
+
+# a.update(name=111)
+# print(a)
+
+# print(a.get("name"))
+# print("               ")
+# print(a["name"])
+
+# # 数组和字典的删除
+
+
+
+# del a["name"]
+
+# print(a)
+
+
+
+
+print("这是一个小练习")
+
+a = input("请输入你的姓名：")
+b= input("请输入你的年龄：")
+c = input("请输入你的性别：")
+
+d={"name":a,"age":b,"sex":c}
+
+
+
+print(d)
